4d5b15f6d357d5308a0ef3821439af14bb25e53e
unknown <stuart@roguex.in>
2011-04-19 02:28:56 +0100

    added main class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * User: Stuart
+ * Date: 19/04/11
+ * Time: 02:28
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Hello world.");
+    }
+
+}
